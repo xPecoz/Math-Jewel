09d5dfa24fb0e5b4dcca82e2de7bc5e0e3661ea4
Peco <119451552+xPecoz@users.noreply.github.com>
2023-08-28 07:41:12 +0300

    Add files via upload

--- classes/js/five.js
+++ classes/js/five.js
@@ -15,33 +15,41 @@ document.onkeydown = (e) => {
 const fiveTrain = [
   {
     name: "B1",
-    href: "https://drive.google.com/file/d/1iBJTxrqnjhlVyqgjLm4O35k22iKATJ16/view?usp=sharing",
+    href: "https://drive.google.com/file/d/1iBJTxrqnjhlVyqgjLm4O35k22iKATJ16/view",
   },
   {
     name: "B2",
-    href: "https://drive.google.com/file/d/1bAeJ3hnduJkrfSb--OPTwN63fJA3_oXm/view?usp=sharing",
+    href: "https://drive.google.com/file/d/1bAeJ3hnduJkrfSb--OPTwN63fJA3_oXm/view",
+  },
+  {
+    name: "B3",
+    href: "https://drive.google.com/file/d/1qb0RZgk37UqZdi-5inAkKxvo759WVJ5M/view",
   },
 ];
 const fiveExam = [
   {
     name: "B1",
-    href: "https://drive.google.com/file/d/1vJn-3cY0vHW2KJHSBjyjZLZKm1UvXQaa/view?usp=sharing",
+    href: "https://drive.google.com/file/d/1vJn-3cY0vHW2KJHSBjyjZLZKm1UvXQaa/view",
   },
   {
     name: "B2",
-    href: "https://drive.google.com/file/d/15A_rwaF92uqQuYqap7nUbPlaVUrbUM47/view?usp=sharing",
+    href: "https://drive.google.com/file/d/15A_rwaF92uqQuYqap7nUbPlaVUrbUM47/view",
+  },
+  {
+    name: "B3",
+    href: "https://drive.google.com/file/d/1paxumgxyZEqNgqQXaaGXXAggmfr7ej_Y/view",
   },
 ];
 const fiveExam_month = [
   {
     name: "أغسطس",
-    href: "https://drive.google.com/file/d/1oe5DF1MDVfPWITQ-YsbyUptqT55MCXnj/view?usp=sharing",
+    href: "#",
   },
 ];
 const fiveExam_final = [
   {
-    name: "الإختبار الثاني",
-    href: "https://drive.google.com/file/d/1oe5DF1MDVfPWITQ-YsbyUptqT55MCXnj/view?usp=sharing",
+    name: "الإختبار الأول",
+    href: "#",
   },
 ];
 const fiveExamDrive = [
@@ -53,17 +61,21 @@ const fiveExamDrive = [
     name: "B2",
     href: "https://docs.google.com/forms/d/1HYmkTFvXTXvf5g4jKoyxfcNBwK2Tp7FvJh4LqLzAhIk/viewform?edit_requested=true",
   },
+  {
+    name: "B3",
+    href: "https://docs.google.com/forms/d/16zO02C6rJOgmBfFH8z3xjPkTzsDDAYa20flLPychoMw/viewform?edit_requested=true",
+  },
 ];
 const fiveExamDrive_final = [
   {
     name: "الإختبار الثاني",
-    href: "https://drive.google.com/file/d/1oe5DF1MDVfPWITQ-YsbyUptqT55MCXnj/view?usp=sharing",
+    href: "#",
   },
 ];
 const fiveExamDrive_month = [
   {
     name: "ديسمبر",
-    href: "https://drive.google.com/file/d/1oe5DF1MDVfPWITQ-YsbyUptqT55MCXnj/view?usp=sharing",
+    href: "#",
   },
 ];
 
@@ -101,8 +113,8 @@ if (
 
 if (location.search == "?train") {
   document.querySelector(".main-page .container").innerHTML = `
-      <h1 class="h1-book">تمارين الرابع</h1>
-      <p class="dec">تمارين الصف الرابع</p>
+      <h1 class="h1-book">تمارين الخامس</h1>
+      <p class="dec">تمارين الصف الخامس</p>
       <h2>جميع الدروس و التمارين</h2>
       <div class="boxs" style="display: grid">
         ${fiveTrain.map((e) => {
@@ -128,9 +140,9 @@ if (location.search == "?train") {
   );
 } else if (location.search == "?exam") {
   document.querySelector(".main-page .container").innerHTML = `
-      <h1 class="h1-book">أختبارات الرابع</h1>
-      <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h1 class="h1-book">أختبارات الخامس</h1>
+      <p class="dec">إختبارات الصف الخامس</p>
+      <h2>جميع إختبارات الصف الخامس</h2>
       <div class="boxs" style="display: grid">
         <div class="box classBox">
           <div class="emty" style="padding: 10px">
@@ -154,9 +166,9 @@ if (location.search == "?train") {
     `;
 } else if (location.search == "?exam&lesson") {
   document.querySelector(".main-page .container").innerHTML = `
-      <h1 class="h1-book">أختبارات الرابع</h1>
-      <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h1 class="h1-book">أختبارات الخامس</h1>
+      <p class="dec">إختبارات الصف الخامس</p>
+      <h2>إختبارات دروس الصف الخامس</h2>
       <div class="boxs" style="display: grid">
         ${fiveExam.map((e) => {
           return `
@@ -181,9 +193,9 @@ if (location.search == "?train") {
   );
 } else if (location.search == "?exam&month") {
   document.querySelector(".main-page .container").innerHTML = `
-      <h1 class="h1-book">أختبارات الرابع</h1>
-      <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h1 class="h1-book">أختبارات الخامس</h1>
+      <p class="dec">إختبارات الصف الخامس</p>
+      <h2>إختبارات شهور الصف الخامس</h2>
       <div class="boxs" style="display: grid">
         ${fiveExam_month.map((e) => {
           return `
@@ -208,9 +220,9 @@ if (location.search == "?train") {
   );
 } else if (location.search == "?exam&final") {
   document.querySelector(".main-page .container").innerHTML = `
-      <h1 class="h1-book">أختبارات الرابع</h1>
-      <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h1 class="h1-book">أختبارات الخامس</h1>
+      <p class="dec">إختبارات الصف الخامس</p>
+      <h2>إختبارات نهائية للصف الخامس</h2>
       <div class="boxs" style="display: grid">
         ${fiveExam_final.map((e) => {
           return `
@@ -235,35 +247,35 @@ if (location.search == "?train") {
   );
 } else if (location.search == "?examDrive") {
   document.querySelector(".main-page .container").innerHTML = `
-      <h1 class="h1-book">أختبارات الرابع</h1>
-      <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h1 class="h1-book">أختبارات الخامس</h1>
+      <p class="dec">إختبارات الصف الخامس</p>
+      <h2>الإختبارات الإلكترونية للصف الخامس</h2>
       <div class="boxs" style="display: grid">
         <div class="box classBox">
           <div class="emty" style="padding: 10px">
             <div class="text">إختبارات الدروس</div>
-            <a href="?exam&lesson" class="coolBeans">الذهاب</a>
+            <a href="?examDrive&lesson" class="coolBeans">الذهاب</a>
           </div>
         </div>
         <div class="box classBox">
           <div class="emty" style="padding: 10px">
             <div class="text">إختبارات الشهور</div>
-            <a href="?exam&month" class="coolBeans">الذهاب</a>
+            <a href="?examDrive&month" class="coolBeans">الذهاب</a>
           </div>
         </div>
         <div class="box classBox">
           <div class="emty" style="padding: 10px">
             <div class="text">إختبارات نهائية</div>
-            <a href="?exam&final" class="coolBeans">الذهاب</a>
+            <a href="?examDrive&final" class="coolBeans">الذهاب</a>
           </div>
         </div>
       </div>
     `;
 } else if (location.search == "?examDrive&lesson") {
   document.querySelector(".main-page .container").innerHTML = `
-      <h1 class="h1-book">أختبارات الرابع</h1>
-      <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h1 class="h1-book">أختبارات الخامس</h1>
+      <p class="dec">إختبارات الصف الخامس</p>
+      <h2>إختبارات إلكترونية لدروس الخامس</h2>
       <div class="boxs" style="display: grid">
         ${fiveExamDrive.map((e) => {
           return `
@@ -288,9 +300,9 @@ if (location.search == "?train") {
   );
 } else if (location.search == "?examDrive&month") {
   document.querySelector(".main-page .container").innerHTML = `
-      <h1 class="h1-book">أختبارات الرابع</h1>
-      <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h1 class="h1-book">أختبارات الخامس</h1>
+      <p class="dec">إختبارات الصف الخامس</p>
+      <h2>إختبارت الشهور الإلكترونية للخامس</h2>
       <div class="boxs" style="display: grid">
         ${fiveExamDrive_month.map((e) => {
           return `
@@ -315,9 +327,9 @@ if (location.search == "?train") {
   );
 } else if (location.search == "?examDrive&final") {
   document.querySelector(".main-page .container").innerHTML = `
-      <h1 class="h1-book">أختبارات الرابع</h1>
-      <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h1 class="h1-book">أختبارات الخامس</h1>
+      <p class="dec">إختبارات الصف الخامس</p>
+      <h2>إختبارات إلكترونية نهائية للخامس</h2>
       <div class="boxs" style="display: grid">
         ${fiveExam_final.map((e) => {
           return `
--- classes/js/four.js
+++ classes/js/four.js
@@ -15,33 +15,41 @@ document.onkeydown = (e) => {
 const fourTrain = [
   {
     name: "A1",
-    href: "https://drive.google.com/file/d/10saF9UpaX752BqnPt10r32Sc1T-Yia0y/view?usp=sharing",
+    href: "https://drive.google.com/file/d/10saF9UpaX752BqnPt10r32Sc1T-Yia0y/view",
   },
   {
     name: "A2",
-    href: "https://drive.google.com/file/d/1beyAKSVUfT0XVm3HvOw1q7ALFQ4Vyw-O/view?usp=sharing",
+    href: "https://drive.google.com/file/d/1beyAKSVUfT0XVm3HvOw1q7ALFQ4Vyw-O/view",
+  },
+  {
+    name: "A3",
+    href: "https://drive.google.com/file/d/1e1t8LSi1s5YyOI5wZPEjEZEVYmlV8gMU/view",
   },
 ];
 const fourExam = [
   {
     name: "A1",
-    href: "https://drive.google.com/file/d/1oe5DF1MDVfPWITQ-YsbyUptqT55MCXnj/view?usp=sharing",
+    href: "https://drive.google.com/file/d/1oe5DF1MDVfPWITQ-YsbyUptqT55MCXnj/view",
   },
   {
     name: "A2",
-    href: "https://drive.google.com/file/d/1F2o8AfKpOLE-8p3U-SQBiw_SyzyvRYPN/view?usp=sharing",
+    href: "https://drive.google.com/file/d/1F2o8AfKpOLE-8p3U-SQBiw_SyzyvRYPN/view",
+  },
+  {
+    name: "A3",
+    href: "https://drive.google.com/file/d/1-08V8Qbs5CMLRSoUUqxxi7ah5t1gkW5A/view",
   },
 ];
 const fourExam_month = [
   {
     name: "اغسطس",
-    href: "https://drive.google.com/file/d/1oe5DF1MDVfPWITQ-YsbyUptqT55MCXnj/view?usp=sharing",
+    href: "#",
   },
 ];
 const fourExam_final = [
   {
     name: "الإختبار الأول",
-    href: "https://drive.google.com/file/d/1oe5DF1MDVfPWITQ-YsbyUptqT55MCXnj/view?usp=sharing",
+    href: "#",
   },
 ];
 const fourExamDrive = [
@@ -53,17 +61,35 @@ const fourExamDrive = [
     name: "A2",
     href: "https://docs.google.com/forms/d/1zAeEKmw_dDAZ85-E3d_T3vYfPj2l27eZisdrljmpA9E/viewform?edit_requested=true",
   },
+  {
+    name: "A3",
+    href: "https://docs.google.com/forms/d/1HuoeypEq2pagB5mA-niydOhdM_1TLpssHcgig9pm1yM/viewform?edit_requested=true",
+  },
 ];
 const fourExamDrive_final = [
   {
     name: "الإختبار الأول",
-    href: "https://drive.google.com/file/d/1oe5DF1MDVfPWITQ-YsbyUptqT55MCXnj/view?usp=sharing",
+    href: "#",
   },
 ];
 const fourExamDrive_month = [
   {
     name: "اغسطس",
-    href: "https://drive.google.com/file/d/1oe5DF1MDVfPWITQ-YsbyUptqT55MCXnj/view?usp=sharing",
+    href: "#",
+  },
+];
+const fourLearnPDF = [
+  {
+    name: "A1",
+    href: "https://drive.google.com/file/d/1RywNEdhA-t3t7jEPCqEu4lG_X82bIOpO/view",
+  },
+  {
+    name: "A2",
+    href: "https://drive.google.com/file/d/1YENh8xbSRTl3YUPk1WtMnxZbYSXsqwXe/view",
+  },
+  {
+    name: "A3",
+    href: "https://drive.google.com/file/d/1a4l6uJumxhWd0qYSzZM0oKQ2dxYH6DFN/view",
   },
 ];
 
@@ -98,6 +124,7 @@ if (
     }
   };
 }
+
 if (location.search == "?train") {
   document.querySelector(".main-page .container").innerHTML = `
       <h1 class="h1-book">تمارين الرابع</h1>
@@ -155,7 +182,7 @@ if (location.search == "?train") {
   document.querySelector(".main-page .container").innerHTML = `
       <h1 class="h1-book">أختبارات الرابع</h1>
       <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h2>إختبارات دروس الصف الرابع</h2>
       <div class="boxs" style="display: grid">
         ${fourExam.map((e) => {
           return `
@@ -182,7 +209,7 @@ if (location.search == "?train") {
   document.querySelector(".main-page .container").innerHTML = `
       <h1 class="h1-book">أختبارات الرابع</h1>
       <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h2>إختبارات شهور الصف الرابع</h2>
       <div class="boxs" style="display: grid">
         ${fourExam_month.map((e) => {
           return `
@@ -209,7 +236,7 @@ if (location.search == "?train") {
   document.querySelector(".main-page .container").innerHTML = `
       <h1 class="h1-book">أختبارات الرابع</h1>
       <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h2>إختبارات نهائية للصف الرابع</h2>
       <div class="boxs" style="display: grid">
         ${fourExam_final.map((e) => {
           return `
@@ -236,24 +263,24 @@ if (location.search == "?train") {
   document.querySelector(".main-page .container").innerHTML = `
       <h1 class="h1-book">أختبارات الرابع</h1>
       <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h2>إختبارات إلكترونية للصف الرابع</h2>
       <div class="boxs" style="display: grid">
         <div class="box classBox">
           <div class="emty" style="padding: 10px">
             <div class="text">إختبارات الدروس</div>
-            <a href="?exam&lesson" class="coolBeans">الذهاب</a>
+            <a href="?examDrive&lesson" class="coolBeans">الذهاب</a>
           </div>
         </div>
         <div class="box classBox">
           <div class="emty" style="padding: 10px">
             <div class="text">إختبارات الشهور</div>
-            <a href="?exam&month" class="coolBeans">الذهاب</a>
+            <a href="?examDrive&month" class="coolBeans">الذهاب</a>
           </div>
         </div>
         <div class="box classBox">
           <div class="emty" style="padding: 10px">
             <div class="text">إختبارات نهائية</div>
-            <a href="?exam&final" class="coolBeans">الذهاب</a>
+            <a href="?examDrive&final" class="coolBeans">الذهاب</a>
           </div>
         </div>
       </div>
@@ -262,7 +289,7 @@ if (location.search == "?train") {
   document.querySelector(".main-page .container").innerHTML = `
       <h1 class="h1-book">أختبارات الرابع</h1>
       <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h2>إختبارات إلكترونية لدروس الرابع</h2>
       <div class="boxs" style="display: grid">
         ${fourExamDrive.map((e) => {
           return `
@@ -289,7 +316,7 @@ if (location.search == "?train") {
   document.querySelector(".main-page .container").innerHTML = `
       <h1 class="h1-book">أختبارات الرابع</h1>
       <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h2>إختبارات إلكترونية لشهور الرابع</h2>
       <div class="boxs" style="display: grid">
         ${fourExamDrive_month.map((e) => {
           return `
@@ -316,9 +343,36 @@ if (location.search == "?train") {
   document.querySelector(".main-page .container").innerHTML = `
       <h1 class="h1-book">أختبارات الرابع</h1>
       <p class="dec">إختبارات الصف الرابع</p>
-      <h2>جميع إختبارات الصف الرابع</h2>
+      <h2>إختبارات إلكترونية نهائية للرابع</h2>
       <div class="boxs" style="display: grid">
         ${fourExam_final.map((e) => {
+          return `
+            <div class="box classBox">
+              <div class="emty" style="padding: 10px">
+                <div class="text">${e.name}</div>
+                <a
+                  target="_blank"
+                  href="${e.href}"
+                  class="coolBeans"
+                >
+                  الذهاب
+                </a>
+              </div>
+            </div>`;
+        })}
+      </div>
+    `.replaceAll(
+    `,
+            `,
+    ""
+  );
+} else if (location.search == "?learnPDF") {
+  document.querySelector(".main-page .container").innerHTML = `
+      <h1 class="h1-book">ملحقات الرابع</h1>
+      <p class="dec">ملحقات الصف الرابع</p>
+      <h2>ملحقات دروس الصف الرابع</h2>
+      <div class="boxs" style="display: grid">
+        ${fourLearnPDF.map((e) => {
           return `
             <div class="box classBox">
               <div class="emty" style="padding: 10px">
